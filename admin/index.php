2d8b8d7756dc087e28d5dcee02eb7ea40bcfa0eb
Ferdinanzk <ferdinanzk@gmail.com>
2023-02-08 07:22:08 +0700

    index

--- admin/index.php
+++ admin/index.php
@@ -67,7 +67,6 @@ th {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="../pemilu/admin.php">Pemilu</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="../login/logout.php">Logout</a></li>
                     </ul>
                 </div>
@@ -371,7 +370,7 @@ th {
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Kotak Pesan</h1>
+            <h1>Table Pemesanan</h1>
           </div>
         </div>
       </div><!-- /.container-fluid -->
